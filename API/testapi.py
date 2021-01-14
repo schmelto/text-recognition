6cde6f98b6d259ee54e7ab7c9561dd7863bbe443
schmelto <tom.schmelzer@web.de>
2021-01-14 17:46:09 +0100

    add post and put method

--- API/testapi.py
+++ API/testapi.py
@@ -5,15 +5,73 @@ from flask_restful import Resource, Api, reqparse
 import pandas as pd
 import ast
 
+userpath = 'C:/Users/tomsc/source/text-recognition/API/users.csv'
 app = Flask(__name__)
 api = Api(app)
 
 class Users(Resource):
     # methods go here
     def get(self):
-        data = pd.read_csv('users.csv')  # read CSV
+        data = pd.read_csv(userpath)  # read CSV
         data = data.to_dict()  # convert dataframe to dictionary
         return {'data': data}, 200  # return data and 200 OK code
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('name', required=True)
+        parser.add_argument('city', required=True)
+        
+        args = parser.parse_args()  # parse arguments to dictionary
+        
+        # create new dataframe containing new values
+        new_data = pd.DataFrame({
+            'userId': args['userId'],
+            'name': args['name'],
+            'city': args['city'],
+            'locations': [[]]
+        })
+        # read our CSV
+        data = pd.read_csv(userpath)
+        # add the newly provided values
+        data = data.append(new_data, ignore_index=True)
+        # save back to CSV
+        data.to_csv(userpath, index=False)
+        return {'data': data.to_dict()}, 200  # return data with 200 OK
+
+    def put(self):
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('userId', required=True)  # add args
+        parser.add_argument('location', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+
+        # read our CSV
+        data = pd.read_csv(userpath)
+        
+        if args['userId'] in list(data['userId']):
+            # evaluate strings of lists to lists
+            data['locations'] = data['locations'].apply(
+                lambda x: ast.literal_eval(x)
+            )
+            # select our user
+            user_data = data[data['userId'] == args['userId']]
+
+            # update user's locations
+            user_data['locations'] = user_data['locations'].values[0] \
+                .append(args['location'])
+            
+            # save back to CSV
+            data.to_csv('users.csv', index=False)
+            # return data and 200 OK
+            return {'data': data.to_dict()}, 200
+
+        else:
+            # otherwise the userId does not exist
+            return {
+                'message': f"'{args['userId']}' user not found."
+            }, 404
+    
     pass
     
 class Locations(Resource):
