564a275a94e5ac2e2598fe3f6abea0c76a0f981d
schmelto <tom.schmelzer@web.de>
2021-01-16 12:11:44 +0100

    add service

--- App/src/app/home/home.page.ts
+++ App/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PhotoService } from '../services/photo.service';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,16 @@ import { PhotoService } from '../services/photo.service';
 })
 export class HomePage {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService, private apiService: ApiService ) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+
+  getModel() {
+    this.apiService.getData().subscribe(data => {
+      console.log(data);
+    });
+
   }
 }
---
+++ App/src/app/services/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// const API_URL = environment.apiUrl;
+const API_URL = 'http://127.0.0.1:5000/model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getData() {
+    return this.http.get(`${API_URL}/model`);
+  }
+}
